97d7e3cc38c4b86e4d0056c06ed4d4e1cc69290a
momokotomoko <momokoffxiv@gmail.com>
2022-07-26 00:47:37 -0400

    Fix repackage
    fix repackage since now gtests also create .exe files so need to specify ffxivoceanfishing.exe specifically or else will package random tests files too

--- repackage.bat
+++ repackage.bat
@@ -1,3 +1,3 @@
 del Release\com.elgato.ffxivoceanfishing.streamDeckPlugin
-xcopy /y Sources\Windows\Release\*.exe Sources\com.elgato.ffxivoceanfishing.sdPlugin\
+xcopy /y Sources\Windows\Release\ffxivoceanfishing.exe Sources\com.elgato.ffxivoceanfishing.sdPlugin\
 DistributionTool.exe -b -i Sources\com.elgato.ffxivoceanfishing.sdPlugin -o Release
